7f34ea32af368ee02ed99ffaa1838fd8d9193726
M.A.N <maximillianaza@gmail.com>
2017-07-12 19:14:09 -0400

    script is finally running successfully, but there’s no output

--- scrape.py
+++ scrape.py
@@ -4,14 +4,14 @@ def get_links(company_name):
 	url = "https://www.google.com/"
 	browser = webdriver.PhantomJS()
 	browser.get(url)
-	search_box = browser.find_element_by_id("list-ib")
+	search_box = browser.find_element_by_name("q")
 	search_box.send_keys(company_name)
 	search_box.submit()
 
 	try:
-		links = browser.find_element_by_xpath("//ol[class='web_regular_results']//h3//a")
+		links = browser.find_elements_by_xpath("//ol[class='web_regular_results']//h3//a")
 	except:
-		links = browser.find_element_by_xpath("//h3//a")
+		links = browser.find_elements_by_xpath("//h3//a")
 		results = []
 
 	for link in links:
