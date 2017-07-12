11fc299c79936d639e801ec1ad783c25086c5f42
M.A.N <maximillianaza@gmail.com>
2017-07-12 19:27:04 -0400

    fixed some syntax errors i had made in my source code. still no output

--- scrape.py
+++ scrape.py
@@ -6,19 +6,19 @@ def get_links(company_name):
 	browser.get(url)
 	search_box = browser.find_element_by_name("q")
 	search_box.send_keys(company_name)
-	search_box.submit()
+	search_box.submit
 
 	try:
 		links = browser.find_elements_by_xpath("//ol[class='web_regular_results']//h3//a")
 	except:
 		links = browser.find_elements_by_xpath("//h3//a")
-		results = []
-
+	results = []
+	
 	for link in links:
 		href = link.get_attribute("href")
 		print(href)
 		results.append(href)
-		browser.close()
-		return results
+	browser.close()
+	return results
 
 get_links("dog")
