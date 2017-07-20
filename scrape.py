ab342bf0f844be9d7446e69ddd9818fbb955b8ad
M.A.N <maximillianaza@gmail.com>
2017-07-19 21:49:02 -0400

    remove trailing whitespace

--- scrape.py
+++ scrape.py
@@ -7,13 +7,11 @@ def get_links(company_name):
 	search_box = browser.find_element_by_name("q")
 	search_box.send_keys(company_name)
 	search_box.submit
-
 	try:
 		links = browser.find_elements_by_xpath("//ol[class='web_regular_results']//h3//a")
 	except:
 		links = browser.find_elements_by_xpath("//h3//a")
 	results = []
-	
 	for link in links:
 		href = link.get_attribute("href")
 		print(href)
